4b4459df07bb71962acb957a753ffd0f66b6bc98
horiaahmed <157916553+horiaahmed@users.noreply.github.com>
2025-04-20 17:52:46 +0200

    test api

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Result extends Model
 {
-    
+    protected $fillable = ['result','video_id'];
     public function video()
 {
     return $this->belongsTo(Video::class);
---
+++ resources/views/video/result.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <h1>Prediction Result</h1>
+
+    <p>Video uploaded successfully. Here is the result of the AI prediction:</p>
+    <p><strong>Result:</strong> {{ $result }}</p>
+</x-layout>
